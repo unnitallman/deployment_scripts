73da172eadcf6d454ee4b152cec990864ea4059c
Unnikrishnan KP <unni.tallman@gmail.com>
2013-01-17 10:03:10 +0530

    bug fixes

--- setup/setup.rb
+++ setup/setup.rb
@@ -3,13 +3,13 @@ require 'helper'
 sites = open("apps.list").read.split("\n")
 
 sources_folder = "/home/dpc/sources"
-targets_folder = "/home/dpc/app/"
+targets_folder = "/home/dpc/app"
 
 branch = "patches"
 
 sites.each do |site|
   source = "#{sources_folder}/#{branch}"
-  target = "#{targets_folder}/app/#{site}/current"
+  target = "#{targets_folder}/#{site}/current"
 
   create_folder target
 
