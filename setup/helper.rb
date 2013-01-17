1b506a9a191fffc669c7daa1f9ebb37881449457
Unnikrishnan KP <unni.tallman@gmail.com>
2013-01-17 09:58:51 +0530

    moved methods into a helper file

---
+++ setup/helper.rb
@@ -0,0 +1,19 @@
+def create_folder(folder_path)
+  `mkdir -p #{folder_path}`
+end
+
+def copy_code(source, target)
+  `cp -r #{source} #{target}`
+end
+
+def copy_routes(target, site)
+  `cp #{target}/config/site_specific_routes/#{site}_routes.rb #{target}/config/site_specific_routes.rb`
+end
+
+def copy_timezone(target, site)
+  `cp #{target}/config/site_specific_timezones/#{site}_timezone.rb #{target}/config/site_specific_timezone.rb`
+end
+
+def copy_database_conf(target, site)
+  `echo 'database: btt_#{site}' > #{target}/config/production_db.yml`
+end
